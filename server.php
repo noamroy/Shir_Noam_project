2b3c7b09372da9e08025d35adc63fd2e0ee9885f
noamroy <80029502+noamroy@users.noreply.github.com>
2021-04-29 15:29:26 +0300

    unite with shir work #1

--- server.php
+++ server.php
@@ -7,7 +7,7 @@
             <section>
                 <h1>the next lines return from the server:</h1>
                 <?php
-
+                    "<h2>PHP is Fun!</h2>";
                 ?>
             </section>
         </body>
