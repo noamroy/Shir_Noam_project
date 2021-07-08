7e7c45e17844c388fa45ff2a775a24aebdd2eefa
shir149 <shiramar0401@gmail.com>
2021-07-08 08:28:24 +0300

    add css and responsivnes

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     include 'db.php';
     include "config.php";
+    include  "css/style.css";
+    
     if(!empty($_POST["loginName"])) { //true if form was submitted
       $query ="SELECT * FROM tbl_87_users WHERE user_name='"
         .$_POST["loginName"]
@@ -23,13 +25,14 @@
 <html>
   <head>
     <meta charset="UTF-8"> 
+    <link rel="stylesheet" href="css/style.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="js/scripts.js"></script>
-    <link rel="stylesheet" href="css/style.css"/>
+    
     <title>Login</title>
   </head>
   <body id = "login">
